8bf0d958cee32118e97f5218047624aa94a1933c
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-03-25 10:37:37 -0400

    Final commit.

--- CodeDevils/src/Main.java
+++ CodeDevils/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,16 +10,10 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-	
-	
-	//static File file = new File ("C:\\Users\\ShaneCupid\\OneDrive - Xpanxion\\Documents\\GitHub\\CodeDevils\\Contacts.txt");
 
 	static HashMap<Integer, Contact> map = new HashMap<>();
 
 	public static void main(String[] args) {
-		
-		
-		// Iterator<Map.Entry<Integer, Contact>> it = map.entrySet().iterator();
 
 		String displayMenu = "[add, list, count, info, delete, search, exit]";
 
@@ -32,46 +27,57 @@ public class Main {
 		String website;
 		Integer id;
 		Contact newContact;
-		
-		File file = new File ("C:\\Users\\JonRodriguez-Castill\\OneDrive - Xpanxion\\Desktop\\GitHub\\CodeDevils\\Contacts.txt");
-		
+
+		File file = new File(
+				"C:\\Users\\JonRodriguez-Castill\\OneDrive - Xpanxion\\Desktop\\GitHub\\CodeDevils\\Contacts.txt");
+
+		// reads the object file. Initiates the nextID attribute in contact class to the
+		// last object written/read on the file and adds 1.
+		// this allows new additions to be added at the end of the list.
 		try {
 			FileInputStream we = new FileInputStream(file);
 			ObjectInputStream ew = new ObjectInputStream(we);
-			
-			Contact person = (Contact) ew.readObject(); 
-			
-			while(person!=null) {
-				
-				
+
+			Contact person = (Contact) ew.readObject();
+
+			while (person != null) {
+
 				map.put(person.getContactId(), person);
-				
+
 				person = (Contact) ew.readObject();
-				
+
+				Contact.nextId = person.getContactId() + 1;
 			}
-			
+
 			ew.close();
-			
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
+
 		}
-		catch(Exception e) {
+
+		catch (EOFException e) {
+			System.out.println("Program has read the Object list.");
+		}
+
+		catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 
-		
-		
-		
-		System.out.println(map);
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// forever Loop to display menu until user enters "exit"
 		while (true) {
 
 			System.out.println(displayMenu);
 			choice = input.nextLine();
 
+			// if statement to create a Person or Organization object.
+
 			if (choice.equalsIgnoreCase("add")) {
 				System.out.println("Enter Person or Organization:");
 				choice = input.nextLine();
 
+				// if statement to create a Person
 				if (choice.equalsIgnoreCase("person")) {
 					System.out.println("Name: ");
 					name = input.nextLine();
@@ -79,6 +85,7 @@ public class Main {
 					System.out.println("Number: ");
 					phone = input.nextLine();
 
+					// if statement to verify phone number format.
 					if (!Contact.isValid(phone)) {
 						phone = "No Number";
 					}
@@ -89,12 +96,14 @@ public class Main {
 					System.out.println("DOB: ");
 					dob = input.nextLine();
 
+					// creates new Person object with user inputed information, and stores it in
+					// Hashmap.
 					newContact = new Person(name, phone, email, dob);
 					map.put(newContact.getContactId(), newContact);
-					
 
 				}
 
+				// if statement to create an Organization
 				else if (choice.equalsIgnoreCase("organization")) {
 
 					System.out.println("Name: ");
@@ -103,6 +112,7 @@ public class Main {
 					System.out.println("Number: ");
 					phone = input.nextLine();
 
+					// verify phone format.
 					if (!Contact.isValid(phone)) {
 						phone = "No Number";
 					}
@@ -110,15 +120,18 @@ public class Main {
 					System.out.println("Website: ");
 					website = input.nextLine();
 
+					// creates new Organization object and stores it in the HashMap.
 					newContact = new Organization(name, phone, website);
 					map.put(newContact.getContactId(), newContact);
-					
+
 				}
 
 			}
 
+			// if user chooses list on the main menu enters this codeblock.
 			else if (choice.equalsIgnoreCase("list")) {
 
+				// for loop that loops through HashMap entries and prints the Id and Name.
 				for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
 					System.out.println(eachContactMapping.getValue().getContactId() + " : "
 							+ eachContactMapping.getValue().getName());
@@ -126,16 +139,17 @@ public class Main {
 
 			}
 
+			// if user enters count in the main menu, prints number of contacts within the
+			// hash map.
 			else if (choice.equalsIgnoreCase("count")) {
 				System.out.println(map.size());
 
 			}
 
+			// if user enters info in the main menu, execute code block.
 			else if (choice.equalsIgnoreCase("info")) {
 
-				// for (int i = 1; i <= map.size(); i++) {
-				// System.out.println(map.get(i));
-
+				// for loop displaying all objects within the hashmap
 				for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
 					System.out.println(eachContactMapping.getValue());
 				}
@@ -145,6 +159,8 @@ public class Main {
 
 					id = input.nextInt();
 
+					// user will input Id of the person they'd like to retrieve the information.
+					// Prints that user info.
 					if (map.containsKey(id)) {
 
 						System.out.println(map.get(id));
@@ -163,32 +179,35 @@ public class Main {
 				}
 			}
 
+			// if user enters delete in main menu execute code block.
 			else if (choice.equalsIgnoreCase("delete")) {
 
-				// for (int i = 1; i <= map.size(); i++) {
-				// System.out.println(map.get(i));
-
+				// for each loop, looping through entire map by Hash Map entries.
 				for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
 					System.out.println(eachContactMapping.getValue());
 				}
 
 				try {
-				System.out.println("Please enter contact ID: ");
+					System.out.println("Please enter contact ID: ");
 
-				id = input.nextInt();
+					id = input.nextInt();
 
-				if (map.containsKey(id)) {
-					map.remove(id);
+					// user will input Id of the person they'd like to delete. Program will delete
+					// that entry from hash map.
+					if (map.containsKey(id)) {
+						map.remove(id);
+
+					}
+
+					// for each loop, looping through entire map by Hash Map entries.
+					for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
+						System.out.println(eachContactMapping.getValue());
+					}
+
+					input.nextLine();
 
 				}
 
-				for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
-					System.out.println(eachContactMapping.getValue());
-				}
-				input.nextLine();
-				
-				}
-				
 				catch (NullPointerException e) {
 
 					System.out.println(e.getStackTrace());
@@ -199,12 +218,16 @@ public class Main {
 				}
 			}
 
+			// if statement if user enters search in the main menu, executes code block.
 			else if (choice.equalsIgnoreCase("search")) {
 
 				System.out.println("Enter your search query");
 
 				choice = input.nextLine().toLowerCase();
 
+				// for each loop that loops through entire Hash Map, gets the Name attribute of
+				// the objects within the map, and
+				// checks if the map contains user input search query
 				for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
 
 					if (eachContactMapping.getValue().getName().contains(choice)) {
@@ -215,34 +238,35 @@ public class Main {
 				}
 			}
 
+			// if user enters exit at any time exits the loop.
 			if (choice.equalsIgnoreCase("exit")) {
 				break;
 			}
 
 		}
-		
+
 		System.out.println("Program Executed");
-		
+
 		try {
-		
-		FileOutputStream fs = new FileOutputStream(file);
-		ObjectOutputStream os = new ObjectOutputStream(fs);
-		
-		for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
-			os.writeObject(eachContactMapping.getValue());
+
+			FileOutputStream fs = new FileOutputStream(file);
+			ObjectOutputStream os = new ObjectOutputStream(fs);
+
+			for (HashMap.Entry<Integer, Contact> eachContactMapping : map.entrySet()) {
+				os.writeObject(eachContactMapping.getValue());
+			}
+
+			os.close();
+
 		}
-		
-		System.out.println(map);
-		os.close();
-		
-		}
-		
-		catch(IOException e) {
+
+		catch (IOException e) {
 			System.out.println(e);
 		}
-		
+
 		input.close();
-		
+
 	}
 
 }
+
